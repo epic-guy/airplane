c6996fa9b82e9c7838b99988f33ebb46bb94cc47
calebgood <samuelwycliffe@gmail.com>
2020-04-23 20:19:49 +0530

    content?

---
+++ nptel_raw.py
@@ -0,0 +1,12 @@
+
+
+'''
+import pandas as pd
+
+df=pd.read_csv('test.csv')
+#df1=Data[['Safety_Score', 'Days_Since_Inspection', 'Turbulence_In_gforces']]
+#df1=Data.loc[:,['Safety_Score', 'Days_Since_Inspection', 'Turbulence_In_gforces']]
+print(df.head())
+df.dropna(axis=0, how='all')
+print(df.head())
+'''
